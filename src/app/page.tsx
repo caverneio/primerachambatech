b0970fdaac56f72dd3d80778efb1097fb4477a69
Anthony Cueva <hi.cuevantn@gmail.com>
2024-03-23 00:17:09 -0500

    feat: vitest and eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import { SonnerDemo } from "./sonner-demo";
 import { ThemePicker } from "./theme-picker";
 
 export default function Home() {
+  const version = "0.0.1";
+
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-8">
       <div className="text-center">
         <h1 className="text-4xl font-bold">shadcn/ui demo</h1>
-        <p>For primerachamba.tech</p>
+        <p>For primerachamba.tech {version}</p>
       </div>
       <ThemePicker />
       <ToggleGroupDemo />
